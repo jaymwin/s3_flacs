e366374b366d6f8ddead608dd2b353539243f08b
jaymwin <jmwiniarski@gmail.com>
2024-02-19 08:40:27 -0600

    clean up code

--- 01-get_S3_bucket.R
+++ 01-get_S3_bucket.R
@@ -3,17 +3,16 @@ library(CAbioacoustics)
 library(tidyverse)
 library(arrow)
 
+# if TRUE, connection is working
 cb_check_s3()
 
 
 # get S3 bucket contents; convert to arrow table --------------------------
 
+# get s3 contents in data frame form
 s3_df <- cb_get_s3_df()
 
-s3_df |>
-  write_rds(here::here('s3_df.rds'))
-
-# arrow table makes querying much faster (and smaller than csv)
+# converting to arrow table makes querying much faster (and smaller than csv)
 s3_arrow <-
   as_arrow_table(s3_df) |>
   select(Key)
@@ -26,10 +25,3 @@ s3_arrow |>
     path = str_c(here::here('s3_bucket_arrow'), '_', Sys.Date()),
     format = 'parquet'
   )
-
-
-s3_pq <- open_dataset(str_c(here::here('s3_bucket_arrow'), '_', Sys.Date()))
-
-s3_pq |>
-  filter(str_detect(Key, '.flac')) |>
-  collect()
