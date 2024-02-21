38d133a947a08824a8c5915b204d3c74933aebeb
jaymwin <jmwiniarski@gmail.com>
2024-02-21 06:56:28 -0600

    updated s3 contents (no longer have different years in 2023 monitoring folder)

--- 02-count_S3_flacs.R
+++ 02-count_S3_flacs.R
@@ -10,34 +10,37 @@ library(CAbioacoustics)
 # open parquet dataset ----------------------------------------------------
 
 # read in parquet data (most recent S3 bucket contents)
-s3_pq <- open_dataset(here::here('s3_bucket_arrow_2024-02-18'))
+s3_pq <- open_dataset(here::here('s3_bucket_arrow_2024-02-19'))
 
 
 # get flac counts ---------------------------------------------------------
 
 # tally flacs by study and year
+studies <- 
+  c(
+    'ARU_Coastal_Barred', 
+    'ARU_Modoc_Projects', 
+    'ARU_Sierra_Removal', 
+    'ARU_Sierra_Monitoring', 
+    'ARU_Sierra_Projects'
+    ) |> 
+  str_flatten(collapse = '|')
+
+# perform tally
 s3_pq |> 
+  # convert from arrow to duck db for easier wrangling
+  to_duckdb() |> 
+  # look at flac files only
   filter(str_detect(Key, '.flac')) |> 
   mutate(
-    study = case_when(
-      # str_extract doesn't work with arrow so use str_detect instead
-      str_detect(Key, 'ARU_Coastal_Barred') ~ 'Coastal Barred',
-      str_detect(Key, 'ARU_Modoc_Projects') ~ 'Modoc Projects',
-      str_detect(Key, 'ARU_Sierra_Removal') ~ 'Sierra Removal',
-      str_detect(Key, 'ARU_Sierra_Monitoring') ~ 'Sierra Monitoring',
-      str_detect(Key, 'ARU_Sierra_Projects') ~ 'Sierra Projects'
-    ),
-    year = case_when(
-      # probably more elegant way to do this with base R functions...
-      str_detect(Key, '/2018/') ~ '2018',
-      str_detect(Key, '/2019/') ~ '2019',
-      str_detect(Key, '/2020/') ~ '2020',
-      str_detect(Key, '/2021/') ~ '2021',
-      str_detect(Key, '/2022/') ~ '2022',
-      str_detect(Key, '/2023/') ~ '2023',
-    )
+    study = regexp_extract(Key, studies),
+    # e.g., '/2021/'
+    year = regexp_extract(Key, "/[0-9]{4}/"),
+    # get actual year out of that
+    year = regexp_extract(year, "[0-9]{4}"), 
   ) |> 
   group_by(study, year) |> 
   tally(name = 'n_flacs') |> 
   # pull into memory now
-  collect()
+  collect() |> 
+  arrange(study, year)
